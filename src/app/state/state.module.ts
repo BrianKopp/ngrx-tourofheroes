5e142f454fa4da0e50cc882b72601b92c37625cc
Kopp <KoppB@atonix.com>
2018-08-08 16:00:04 -0500

    Updated state module

--- src/app/state/state.module.ts
+++ src/app/state/state.module.ts
@@ -1,10 +1,31 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Optional, SkipSelf, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { routerReducer, StoreRouterConnectingModule } from '@ngrx/router-store';
+
+import { environment } from '../../environments/environment';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    StoreModule.forRoot({routerReducer: routerReducer}),
+    StoreRouterConnectingModule.forRoot(),
+    !environment.production ? StoreDevtoolsModule.instrument() : [],
   ],
   declarations: []
 })
-export class StateModule { }
+export class StateModule {
+  constructor(@Optional() @SkipSelf() parentModule: StateModule) {
+    if (parentModule) {
+      throw new Error('StateModule is already loaded. Import it in the AppModule only');
+    }
+  }
+  static forRoot(): ModuleWithProviders {
+    return {
+      ngModule: StateModule,
+      providers: []
+    }
+  }
+}
